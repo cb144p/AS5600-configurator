308cc7389ab12b13e368abd3ce678f029dd59bf6
cb144 <orangecw10@gmail.com>
2025-01-28 20:54:38 -0500

    Added a thing.

--- blackpill/src/I2CAS5600.h
+++ blackpill/src/I2CAS5600.h
@@ -1,20 +1,143 @@
 #ifndef I2CAS5600_H
 #define I2CAS5600_H
 
+#pragma GCC optimize ("O3")
+
 #include <stdint.h>
 #include <stm32f411xe.h>
+#include <array>
+#include <algorithm>
+#include <string_view>
+#include <bitset>
 
 #include "i2c.h"
 #include "itimer.h"
 
+#define SAFE_FMAP
+template<typename Key, typename Value, uint8_t size>
+struct fMap {
+	std::array<std::pair<Key, Value>, size> iMap;
+	__attribute__((optimize ("03"))) constexpr Value at(const Key &key) const {
+    const auto itr =
+        std::find_if(std::begin(iMap), std::end(iMap),
+                     [&key](const auto &v) { return v.first == key; });
+    	#ifdef SAFE_FMAP
+			if (itr != std::end(data)) {
+    			return itr->second;
+    		} else {
+    			return 255;
+    		}
+		#else
+			return itr->second;
+		#endif // SAFE_FMAP
+  	}
+};
+
 constexpr uint8_t AS5600ID = 0x36;
-constexpr uint8_t ANGLEID = 0x0E;
-constexpr uint8_t RAWANGLEID = 0x0C;
-constexpr uint8_t STATUSID = 0x0B;
+
+namespace regs {
+    enum {
+        ZMCO = 0x00,
+        ZPOS = 0x01,
+        MPOS = 0x03,
+        MANG = 0x05,
+        CONF = 0x07,
+        RAWANGLE = 0x0C,
+        ANGLE = 0x0E,
+        STATUS = 0x0B,
+        AGC = 0x1A,
+        MAGNITUDE = 0x1B,
+        BURN = 0xFF
+    };
+
+    constexpr uint8_t length = 11;
+    // Lazy, but it works
+    std::array<std::pair<const std::string_view, const uint8_t>, regs::length> nameArray{{
+        {"ZMCO", 0},
+        {"ZPOS", 1},
+        {"MPOS", 2},
+        {"MANG", 3},
+        {"CONF", 4},
+        {"RAWANGLE", 5},
+        {"ANGLE", 6},
+        {"STATUS", 7},
+        {"AGC", 8},
+        {"MAGNITUDE", 9},
+        {"BURN", 10}
+    }};
+
+    fMap<const std::string_view, const uint8_t, length> nameToIndex{{nameArray}};
+
+    constexpr std::bitset<length> sizes{0b01001111110};
+
+    constexpr uint8_t addrs[length]{
+        ZMCO,
+        ZPOS,
+        MPOS,
+        MANG,
+        CONF,
+        RAWANGLE,
+        ANGLE,
+        STATUS,
+        AGC,
+        MAGNITUDE,
+        BURN
+    };
+
+}
+
+namespace conf {
+    enum {
+        PM = 0x0,
+        HYST = 0x2,
+        OUTS = 0x4,
+        PWMF = 0x6,
+        SF = 0x8,
+        FTH = 0xA,
+        WD = 0xD
+    };
+    constexpr uint8_t length = 7;
+
+    std::array<std::pair<const std::string_view, const uint8_t>, length> nameArray = {{
+        {"PM", 0},
+        {"HYST", 1},
+        {"OUTS", 2},
+        {"PWMF", 3},
+        {"SF", 4},
+        {"FTH", 5},
+        {"WD", 6}
+    }};
+
+    fMap<const std::string_view, const uint8_t, length> nameToIndex{{nameArray}};
+}
+
+namespace status {
+    enum {
+        MH = 0x3,
+        ML = 0x4,
+        MD = 0x5
+    };
+    constexpr uint8_t length = 3;
+
+    std::array<std::pair<const std::string_view, const uint8_t>, length> nameArray = {{
+        {"MD", 0},
+        {"ML", 1},
+        {"MD", 2}
+    }};
+
+    fMap<const std::string_view, const uint8_t, length> nameToIndex{{nameArray}}; 
+}
+
+struct AS5600Configs {
+    uint16_t ZPOS;
+    uint16_t MPOS;
+    uint16_t MANG;
+    uint16_t CONF;
+};
 
 // Only one per bus
 class I2CAS5600 {
-    
+
 public:
     I2C_TypeDef* i2c;
     uint8_t status;
@@ -43,7 +166,7 @@ public:
     }
 
     uint16_t readRawAngle() {
-        i2c_startup(RAWANGLEID);
+        i2c_startup(regs::RAWANGLE);
         uint8_t pos[2];
         
         i2c_read(i2c, AS5600ID, pos, 2);
@@ -51,7 +174,7 @@ public:
     }
 
     uint16_t readAngle() {
-        i2c_startup(ANGLEID);
+        i2c_startup(regs::ANGLE);
         uint8_t pos[2];
         
         i2c_read(i2c, AS5600ID, pos, 2);
@@ -59,7 +182,7 @@ public:
     }
 
     void readStatus() {
-        i2c_startup(STATUSID);
+        i2c_startup(regs::STATUS);
         status = i2c_read(i2c, AS5600ID);
     }
 
--- blackpill/src/main.cpp
+++ blackpill/src/main.cpp
@@ -5,42 +5,131 @@
 #include "gpio.h"
 #include "rcc.h"
 #include "systick.h"
-#include "itimer.h"
+#include "usart.h"
+#include "I2CAS5600.h"
+#include <cstring>
+#include <alloca.h>
 
 void SystemInit() {
-    SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); // enable fpu
-    FLASH->ACR |= 3UL | FLASH_ACR_PRFTEN;
-    RCC->CR |= RCC_CR_HSEON;
-    while (!(RCC->CR & RCC_CR_HSERDY_Msk));
-    RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE | PLL_P << RCC_PLLCFGR_PLLP_Pos | PLL_Q << RCC_PLLCFGR_PLLQ_Pos | PLL_N << RCC_PLLCFGR_PLLN_Pos | PLL_M << RCC_PLLCFGR_PLLM_Pos;
-    RCC->CFGR |= HPRE << RCC_CFGR_HPRE_Pos | PPRE1 << RCC_CFGR_PPRE1_Pos | PPRE2 << RCC_CFGR_PPRE2_Pos | RTCPRE << RCC_CFGR_RTCPRE_Pos;
-    RCC->CR |= RCC_CR_PLLON_Msk;
-    while (!(RCC->CR & RCC_CR_PLLRDY_Msk));
-    RCC->CFGR |= SW;
-    systick_init(1000);
+	SCB->CPACR |= ((3UL << 10 * 2) | (3UL << 11 * 2)); // enable fpu
+	FLASH->ACR |= 3UL | FLASH_ACR_PRFTEN;
+	RCC->CR |= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY_Msk));
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE | PLL_P << RCC_PLLCFGR_PLLP_Pos | PLL_Q << RCC_PLLCFGR_PLLQ_Pos | PLL_N << RCC_PLLCFGR_PLLN_Pos | PLL_M << RCC_PLLCFGR_PLLM_Pos;
+	RCC->CFGR |= HPRE << RCC_CFGR_HPRE_Pos | PPRE1 << RCC_CFGR_PPRE1_Pos | PPRE2 << RCC_CFGR_PPRE2_Pos | RTCPRE << RCC_CFGR_RTCPRE_Pos;
+	RCC->CR |= RCC_CR_PLLON_Msk;
+	while (!(RCC->CR & RCC_CR_PLLRDY_Msk));
+	RCC->CFGR |= SW;
+	systick_init(1000);
 }
 
+// Buffer Stuffer
+constexpr uint8_t bufSize = 32;
+char buffer[bufSize];
+uint8_t bufIndex = 0;
+uint16_t config[4]{};
+std::bitset<4> configWriteStatus;
+
+USART_TypeDef* uart = USART2;
+
+I2CAS5600 encoder{I2C1};
+
+const char* commands[] = {
+	"set",
+	"write",
+	"read",
+	"clear",
+	"burn"
+};
+
+void parse_command() {
+	uint8_t size = 0;
+
+	char* token = strtok(buffer, " ");
+	size = strlen(token);
+
+	char log[100];
+
+	uint8_t i;
+	for (i = 0; i < sizeof(commands); i++) {
+		if (token == nullptr) {
+			i = sizeof(commands) + 1;
+			break;
+		}
+		if (strlen(commands[i]) == size) {
+			if (strncmp(commands[i], token, size)) {
+				continue;
+			}
+			switch (i) {
+				case 0:
+					token = strtok(nullptr, " ");
+					uint8_t regIndex = regs::nameToIndex.at(std::string_view{token, size});
+					
+					if (regIndex == 0 || regIndex > 4) {
+						uart_write_buf(uart, "Error: invalid register, stopping", 23);
+						i = sizeof(commands) + 2;
+						continue;
+					}
+					token = strtok(nullptr, " ");
+					char* temp;
+					config[regIndex - 1] = (uint16_t)strtol(token, &temp, 2);
+					if (*temp != '\0' || !strcmp(token, temp)) {
+						uart_write_buf(uart, "Error, invalid number.\n", 23);
+						i = sizeof(commands) + 2;
+						continue;
+					}
+					sprintf(log, "Wrote %u to %s.\n", config[regIndex - 1], regs::nameArray[regIndex].first.data());
+					uart_write_buf(uart, log, strlen(log));
+					token = strtok(nullptr, " ");
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+			}
+		}
+		else {
+			continue;
+		}
+	}
+
+	if (i == sizeof(commands)) {
+		uart_write_buf(uart, "Error: Couldn't recognize command.\n", 23);
+	}
+	else if (i == sizeof(commands + 1)) {
+		uart_write_buf(uart, "Completed commands successfully.\n", 33);
+	}
+
+}
 
 int main() {
-    SystemInit();
+	SystemInit();
 
-    ITimer stim = ITimer::periodicInit(TIM3, 1000);
-    stim.enableInterrupt();
+	uint16_t ledB = PIN('C', 13);
+	gpio_set_mode(ledB, GPIO_MODE_OUTPUT, GPIO_OTYPE_PUSH_PULL, GPIO_OSPEED_LOW_SPEED, GPIO_PUPD_NONE);
+	bool ledState = true;
+	gpio_write(ledB, !ledState);
+	ledState = !ledState;
+	
+	uart_init(uart, 115200);
 
+	uint32_t timer = 0, period = 2000;
+	
+	while (true) {
+		if (timer_expired(&timer, period, s_ticks)) {
+			gpio_write(ledB, !ledState);
+			ledState = !ledState;
+		}
 
-
-    uint16_t ledB = PIN('C', 13);
-    gpio_set_mode(ledB, GPIO_MODE_OUTPUT, GPIO_OTYPE_PUSH_PULL, GPIO_OSPEED_LOW_SPEED, GPIO_PUPD_NONE);
-    bool ledState = true;
-    gpio_write(ledB, !ledState);
-    ledState = !ledState;
-    
-
-    uint32_t timer = 0, period = 2000;
-    while (true) {
-        if (timer_expired(&timer, period, s_ticks)) {
-            gpio_write(ledB, !ledState);
-            ledState = !ledState;
-        }
-    }
+		if (uart_read_ready(uart)) {
+			buffer[bufIndex] = uart_read_byte(uart);
+			if (buffer[bufIndex] == '\n') {
+				buffer[bufIndex] = '\0';
+			}
+		}
+	}
 }
